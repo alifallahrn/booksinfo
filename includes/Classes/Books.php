8e0fd2d5229f0cfd1c24d1a881082a144153ef30
Ali Fallah <ali.fallah.rn@gmail.com>
2018-07-29 16:16:05 +0430

    added isbn metabox to book post type and save (isbn & post_id) in books_info table

--- includes/Classes/Books.php
+++ includes/Classes/Books.php
@@ -12,6 +12,7 @@ class Books
 	{
 		self::register_post_type();
 		self::register_taxonomy();
+		self::hooks();
 	}
 
 	public static function register_post_type()
@@ -59,5 +60,54 @@ class Books
 			'add_new_item' => __('Add New Publisher', Books_Info::DOMAIN),
 			'search_items' => __('Search Publishers', Books_Info::DOMAIN),
 		]);
+	}
+
+	public static function hooks()
+	{
+		add_action('add_meta_boxes_'.self::$post_type, array(__CLASS__, 'add_meta_boxes'));
+		add_action('save_post_'.self::$post_type, array(__CLASS__, 'save_post'));
+	}
+
+	public static function add_meta_boxes()
+	{
+		add_meta_box('book-isbn-meta-box', __('Isbn', Books_Info::DOMAIN), array(__CLASS__, 'book_isbn_meta_box'));
+	}
+
+	public static function book_isbn_meta_box($post)
+	{
+		wp_nonce_field('book_isbn_notice_nonce', 'book_isbn_notice_nonce');
+		$isbn = self::get_isbn_from_db($post->ID);
+		echo '<input type="text" name="_isbn" value="'.$isbn.'" style="width: 100%; text-align: center;" dir="ltr" placeholder="'.__('Enter Book ISBN Here', Books_Info::DOMAIN).'" />';
+	}
+
+	public static function save_post($post_id)
+	{
+		if ( ! isset( $_POST['book_isbn_notice_nonce'] ) ) return;
+		if ( ! wp_verify_nonce( $_POST['book_isbn_notice_nonce'], 'book_isbn_notice_nonce' ) ) return;
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) return;
+		if ( ! current_user_can( 'edit_page', $post_id ) ) return;
+		if ( ! isset( $_POST['_isbn'] ) ) return;
+		$isbn = sanitize_text_field( $_POST['_isbn'] );
+		self::save_isbn_to_db($post_id, $isbn);
+	}
+
+	public static function save_isbn_to_db($post_id, $isbn)
+	{
+		global $wpdb;
+		$table = $wpdb->prefix.Books_Info::TABLE_NAME;
+		$check_isbn = self::get_isbn_from_db($post_id);
+		if(!$check_isbn) {
+			$wpdb->insert($table, array('post_id' => $post_id, 'isbn' => $isbn));
+		} else {
+			$wpdb->update($table, array('isbn' => $isbn), array('post_id' => $post_id));
+		}
+	}
+
+	public static function get_isbn_from_db($post_id)
+	{
+		global $wpdb;
+		$table = $wpdb->prefix.Books_Info::TABLE_NAME;
+		$isbn = $wpdb->get_var('SELECT isbn FROM '.$table.' WHERE post_id="'.$post_id.'"');
+		return $isbn;
 	}
 }
